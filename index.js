297b3082df59066d791aa1e4c88ad8067a3e1f59
Matt Dudzik <mdudzik92@gmail.com>
2020-08-28 12:00:07 -0500

    start on generator with one readme created

--- index.js
+++ index.js
@@ -1,15 +1,39 @@
+const fs = require("fs")
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown.js")
 // array of questions for user
 const questions = [
-
+{
+    type: "input",
+    name: "title",
+    message: "title"
+},
+{
+    type: "input",
+    name: "description",
+    message: "Add your description"
+},
+{
+    type: "list",
+    name: "license",
+    message: "Choose the correct license for your project",
+    choices: ["MIT", "BSD", "None"]
+}
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+fs.writeFileSync(fileName, data)
 }
 
 // function to initialize program
 function init() {
-
+    inquirer.prompt(questions).then(function(responses){
+        console.log(responses)
+    const data = generateMarkdown(responses)
+    writeToFile("newreadme.md", data)
+    })
+    
 }
 
 // function call to initialize program
---
+++ newreadme.md
@@ -0,0 +1,3 @@
+# Good README
+  # App which creates a readme
+  # MIT
